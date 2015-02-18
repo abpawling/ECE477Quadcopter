3a9236cd5f0644d8d4a7647ded2aa7595f16ff6a
Andrew Pawling <apawling@purdue.edu>
2015-02-18 10:04:05 -0500

    first commit

---
+++ configuration_bits.c
@@ -0,0 +1,82 @@
+/******************************************************************************/
+/* Files to Include                                                           */
+/******************************************************************************/
+
+/* Device header file */
+#if defined(__XC16__)
+    #include <xc.h>
+#elif defined(__C30__)
+    #if defined(__PIC24E__)
+    	#include <p24Exxxx.h>
+    #elif defined (__PIC24F__)||defined (__PIC24FK__)
+	#include <p24Fxxxx.h>
+    #elif defined(__PIC24H__)
+	#include <p24Hxxxx.h>
+    #endif
+#endif
+
+/******************************************************************************/
+/* Configuration Bits                                                         */
+/*                                                                            */
+/* This is not all available configuration bits for all PIC24 devices.        */
+/* Refer to the PIC24 device specific .h file in the compiler                 */
+/* support\PIC24x\h (x=F,H,E) directory for complete options specific to the  */
+/* selected device.  For additional information about what the hardware       */
+/* configurations mean in terms of device operation, refer to the device      */
+/* datasheet 'Special Features' chapter.                                      */
+/*                                                                            */
+/* A feature of MPLAB X is the 'Generate Source Code to Output' utility in    */
+/* the Configuration Bits window.  Under Window > PIC Memory Views >          */
+/* Configuration Bits, a user controllable configuration bits window is       */
+/* available to Generate Configuration Bits source code which the user can    */
+/* paste into this project.                                                   */
+/******************************************************************************/
+
+/* TODO Fill in your configuration bits from the config bits generator here.  */
+
+// PIC24EP512GU810 Configuration Bit Settings
+
+// 'C' source line config statements
+
+#include <xc.h>
+
+// FGS
+#pragma config GWRP = OFF               // General Segment Write-Protect bit (General Segment may be written)
+#pragma config GSS = OFF                // General Segment Code-Protect bit (General Segment Code protect is disabled)
+#pragma config GSSK = OFF               // General Segment Key bits (General Segment Write Protection and Code Protection is Disabled)
+
+// FOSCSEL
+#pragma config FNOSC = FRCDIVN          // Initial Oscillator Source Selection bits (Internal Fast RC (FRC) Oscillator with postscaler)
+#pragma config IESO = ON                // Two-speed Oscillator Start-up Enable bit (Start up device with FRC, then switch to user-selected oscillator source)
+
+// FOSC
+#pragma config POSCMD = NONE            // Primary Oscillator Mode Select bits (Primary Oscillator disabled)
+#pragma config OSCIOFNC = OFF           // OSC2 Pin Function bit (OSC2 is clock output)
+#pragma config IOL1WAY = ON             // Peripheral pin select configuration (Allow only one reconfiguration)
+#pragma config FCKSM = CSDCMD           // Clock Switching Mode bits (Both Clock switching and Fail-safe Clock Monitor are disabled)
+
+// FWDT
+#pragma config WDTPOST = PS32768        // Watchdog Timer Postscaler bits (1:32,768)
+#pragma config WDTPRE = PR128           // Watchdog Timer Prescaler bit (1:128)
+#pragma config PLLKEN = ON              // PLL Lock Wait Enable bit (Clock switch to PLL source will wait until the PLL lock signal is valid.)
+#pragma config WINDIS = OFF             // Watchdog Timer Window Enable bit (Watchdog Timer in Non-Window mode)
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable bit (Watchdog timer enabled/disabled by user software)
+
+// FPOR
+#pragma config FPWRT = PWR128           // Power-on Reset Timer Value Select bits (128ms)
+#pragma config BOREN = ON               // Brown-out Reset (BOR) Detection Enable bit (BOR is enabled)
+#pragma config ALTI2C1 = OFF            // Alternate I2C pins for I2C1 (SDA1/SCK1 pins are selected as the I/O pins for I2C1)
+#pragma config ALTI2C2 = OFF            // Alternate I2C pins for I2C2 (SDA2/SCK2 pins are selected as the I/O pins for I2C2)
+
+// FICD
+#pragma config ICS = PGD1               // ICD Communication Channel Select bits (Communicate on PGEC1 and PGED1)
+#pragma config RSTPRI = PF              // Reset Target Vector Select bit (Device will obtain reset instruction from Primary flash)
+#pragma config JTAGEN = OFF             // JTAG Enable bit (JTAG is disabled)
+
+// FAS
+#pragma config AWRP = OFF               // Auxiliary Segment Write-protect bit (Aux Flash may be written)
+#pragma config APL = OFF                // Auxiliary Segment Code-protect bit (Aux Flash Code protect is disabled)
+#pragma config APLK = OFF               // Auxiliary Segment Key bits (Aux Flash Write Protection and Code Protection is Disabled)
+
+
+
