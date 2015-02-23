11bb0a8369d10ad17ceca7cd67cdba4f0d01a57a
Zach Schaffter <zschafft@purdue.edu>
2015-02-22 22:00:34 -0500

    brainstorming

--- user.c
+++ user.c
@@ -17,8 +17,11 @@
 
 #include <stdint.h>          /* For uint32_t definition */
 #include <stdbool.h>         /* For true/false definition */
+#include <stdlib.h>
+
 
 #include "user.h"            /* variables/params used by user.c */
+#include "LinkedList.h"
 
 /******************************************************************************/
 /* User Functions                                                             */
@@ -35,3 +38,45 @@ void InitApp(void)
     /* Initialize peripherals */
 }
 
+waypoint * InitCollisionList(int coord)
+{
+    /*
+    //declarations
+    waypoint *head = NULL;
+    waypoint *current;
+    int i;
+
+    #define COLLISION_COUNT 20
+    static waypoint collisions [COLLISION_COUNT];
+
+    //get current GPS waypoint
+    //data = getCurrPos();
+
+     */
+    
+}
+
+ bool Navigate(int finalDest)
+{
+    //declarations
+     char orientation;
+
+     
+     /*if (orientation == "N" && finalDest.orientation != "N")
+     {
+        //rotate until facing orientation
+
+     }*/
+
+    //generate linked list for traversal
+    /*for (i = 0;i <= 5;i++)
+    {
+        current = (waypoint* )malloc(sizeof(waypoint));
+        current->coordinate = i//data + 4;
+        current->next = head;
+        head = current;
+    }
+
+    current = head;*/
+    
+ }
--- user.h
+++ user.h
@@ -11,3 +11,5 @@
 /* TODO User level functions prototypes (i.e. InitApp) go here */
 
 void InitApp(void);         /* I/O and Peripheral Initialization */
+waypoint * InitCollisionList(int coord); /*Initialize the list that keeps track of collisions for return flight path*/
+bool Navigate(int finalDest); /*GPS Waypoint navigation function*/
